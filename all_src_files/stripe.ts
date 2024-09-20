e0600ff4c1bf9e446b81441660f8170b48ea1b30
toema <mimo.saif20202@gmail.com>
2024-09-20 03:54:53 +0300

    adding pre-hooks for file paths

---
+++ all_src_files/stripe.ts
@@ -0,0 +1,10 @@
+// File: all_src_files/stripe.ts
+export type StripeSubscriptionStatus = 
+  | 'incomplete'
+  | 'incomplete_expired'
+  | 'trialing'
+  | 'active'
+  | 'past_due'
+  | 'canceled'
+  | 'unpaid'
+  | 'paused';
---
+++ src/types/stripe.ts
@@ -0,0 +1,10 @@
+// File: src/types/stripe.ts
+export type StripeSubscriptionStatus = 
+  | 'incomplete'
+  | 'incomplete_expired'
+  | 'trialing'
+  | 'active'
+  | 'past_due'
+  | 'canceled'
+  | 'unpaid'
+  | 'paused';
